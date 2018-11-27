b88aa1e7c4fdf02bcf6993d8926f2697c2acf248
Kutimotitokura <kutimoti.tokura@gmail.com>
2018-11-28 08:09:52 +0900

    [update] graph includes undirected_graph and directed_graph impl

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,7 +1,5 @@
 pub mod property;
 pub mod matching;
-mod undirected_graph;
-mod directed_graph;
 
 pub mod shortest_path;
 
@@ -74,3 +72,77 @@ pub struct UndirectedGraph<VP: Property,EP: Property> {
     es : Vec<EP>,
     vs : Vec<VP>
 }
+
+impl<VP : Property ,EP : Property> Graph for DirectedGraph<VP,EP> {
+    type VP = VP;
+    type EP = EP;
+    fn new(n : usize , vp_init: VP) -> Self {
+        DirectedGraph {
+            n: n,
+            m: 0,
+            g: vec![Vec::<Edge>::new(); n],
+            es: Vec::<EP>::new(),
+            vs: vec![vp_init; n]
+        }
+    }
+    fn add_edge(&mut self , from : &Vertex , to : &Vertex , edge_prop : Self::EP) {
+        self.g[from.0].push(Edge{index : self.m, from : from.clone() , to : to.clone()});
+        self.es.push(edge_prop);
+        self.m += 1;
+    }
+    fn vertices_cnt(&self) -> usize { self.n }
+    fn edges_cnt(&self) -> usize { self.m }
+    fn delta(&self , v : &Vertex) -> Iter<Edge> {
+        self.g[v.0].iter()
+    }
+    fn vprop_mut(&mut self, v : &Vertex) -> &mut Self::VP {
+        &mut self.vs[v.0]
+    }
+    fn vprop(&self, v : &Vertex) -> &Self::VP {
+        & self.vs[v.0]
+    }
+    fn eprop_mut(&mut self, e : &Edge) -> &mut Self::EP {
+        &mut self.es[e.index]
+    }
+    fn eprop(&self, e : &Edge) -> &Self::EP {
+        & self.es[e.index]
+    }
+}
+
+impl<VP : Property ,EP : Property> Graph for UndirectedGraph<VP,EP> {
+    type VP = VP;
+    type EP = EP;
+    fn new(n : usize , vp_init: VP) -> Self {
+        UndirectedGraph {
+            n: n,
+            m: 0,
+            g: vec![Vec::<Edge>::new(); n],
+            es: Vec::<EP>::new(),
+            vs: vec![vp_init; n]
+        }
+    }
+    fn add_edge(&mut self , from : &Vertex , to : &Vertex , edge_prop : Self::EP) {
+        self.g[from.0].push(Edge{index : self.m , from : from.clone() , to : to.clone()});
+        self.g[to.0].push(Edge{index : self.m, from : to.clone(), to : from.clone()});
+        self.es.push(edge_prop);
+        self.m += 1;
+    }
+    fn vertices_cnt(&self) -> usize { self.n }
+    fn edges_cnt(&self) -> usize { self.m }
+    fn delta(&self , v : &Vertex) -> Iter<Edge> {
+        self.g[v.0].iter()
+    }
+    fn vprop_mut(&mut self, v : &Vertex) -> &mut Self::VP {
+        &mut self.vs[v.0]
+    }
+    fn vprop(&self, v : &Vertex) -> &Self::VP {
+        & self.vs[v.0]
+    }
+    fn eprop_mut(&mut self, e : &Edge) -> &mut Self::EP {
+        &mut self.es[e.index]
+    }
+    fn eprop(&self, e : &Edge) -> &Self::EP {
+        & self.es[e.index]
+    }
+}
+

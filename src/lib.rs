360cb74125394abca4b7029b07ea2a56e5dc4647
Kutimotitokura <kutimoti.tokura@gmail.com>
2018-12-02 20:46:53 +0900

    [update] set up dijkstra test on circle ci

--- src/lib.rs
+++ src/lib.rs
@@ -1,110 +1,37 @@
 pub mod graph;
 
+extern crate rand;
 
 #[cfg(test)]
 mod tests {
     use graph::*;
-    use graph::dynamic_directed_graph::*;
+    use graph::directed_graph::*;
+    use graph::shortest_path::bellman_ford::*;
+    use graph::shortest_path::dijkstra::*;
+    use rand::Rng;
 
     #[test]
-    fn test2() {
-        println!("test2 start");
-        let mut g = DynamicDirectedGraph::<usize,usize>::new();
-        g.add_vertex(&Vertex(1), 0);
-        g.add_vertex(&Vertex(2), 0);
-        g.add_vertex(&Vertex(3), 0);
-        g.add_vertex(&Vertex(4), 0);
-
-        g.add_edge(&Vertex(1),&Vertex(2),0);
-        g.add_edge(&Vertex(2),&Vertex(3),0);
-        g.add_edge(&Vertex(3),&Vertex(4),0);
-        g.add_edge(&Vertex(4),&Vertex(1),0);
-        for v in 1..=4 {
-            println!("from {}" , v);
-            for e in g.delta(&Vertex(v)) {
-                println!("{}" , e.to.0);
-            }
+    fn dijkstra_test() {
+        let v = 100;
+        let e = 300;
+        let mut g = DirectedGraph::<usize,usize>::new(v,0);
+        for _ in 0..e {
+            let a = rand::thread_rng().gen_range(0,v);
+            let b = rand::thread_rng().gen_range(0,v);
+            let w = rand::thread_rng().gen_range(1,1001);
+            g.add_edge(&Vertex(a), &Vertex(b), w);
         }
-        println!("remove (2,3)");
-        g.erase_edge(&Edge{from: Vertex(2) , to: Vertex(3) , index: 1});
-        for v in 1..=4 {
-            println!("from {}" , v);
-            for e in g.delta(&Vertex(v)) {
-                println!("{}" , e.to.0);
+        let bf_res = bellman_ford(&g, Vertex(0), 0, |w| w);
+        let di_res = dijkstra(&g, Vertex(0), 0, |w| w);
+        let ans: Vec<Option<usize>> = bf_res.iter().map(|r| {
+            match r {
+                BFResult::Some(w) => Some(*w),
+                _ => None
             }
-        }
-        g.add_edge(&Vertex(2),&Vertex(3),0);
-
-        println!("remove vertex 2");
-
-        g.erase_vertex(&Vertex(2));
-        
-        for v in 1..=4 {
-            if v == 2 { continue; }
-            println!("from {}" , v);
-            for e in g.delta(&Vertex(v)) {
-                println!("{}" , e.to.0);
-            }
-        }
-
-        println!("add vertex 2");
-        g.add_vertex(&Vertex(2),0);
-        for v in 1..=4 {
-            println!("from {}" , v);
-            for e in g.delta(&Vertex(v)) {
-                println!("{}" , e.to.0);
-            }
-        }
-    }
-    use graph::dynamic_undirected_graph::*;
-
-    #[test]
-    fn test3() {
-        println!("test3 start");
-        let mut g = DynamicUndirectedGraph::<usize,usize>::new();
-        g.add_vertex(&Vertex(1), 0);
-        g.add_vertex(&Vertex(2), 0);
-        g.add_vertex(&Vertex(3), 0);
-        g.add_vertex(&Vertex(4), 0);
-
-        g.add_edge(&Vertex(1),&Vertex(2),0);
-        g.add_edge(&Vertex(2),&Vertex(3),0);
-        g.add_edge(&Vertex(3),&Vertex(4),0);
-        g.add_edge(&Vertex(4),&Vertex(1),0);
-        for v in 1..=4 {
-            println!("from {}" , v);
-            for e in g.delta(&Vertex(v)) {
-                println!("{}" , e.to.0);
-            }
-        }
-        println!("remove (2,3)");
-        g.erase_edge(&Edge{from: Vertex(2) , to: Vertex(3) , index: 1});
-        for v in 1..=4 {
-            println!("from {}" , v);
-            for e in g.delta(&Vertex(v)) {
-                println!("{}" , e.to.0);
-            }
-        }
-        g.add_edge(&Vertex(2),&Vertex(3),0);
-
-        println!("remove vertex 2");
-
-        g.erase_vertex(&Vertex(2));
-        
-        for v in 1..=4 {
-            if v == 2 { continue; }
-            println!("from {}" , v);
-            for e in g.delta(&Vertex(v)) {
-                println!("{}" , e.to.0);
-            }
-        }
-
-        println!("add vertex 2");
-        g.add_vertex(&Vertex(2),0);
-        for v in 1..=4 {
-            println!("from {}" , v);
-            for e in g.delta(&Vertex(v)) {
-                println!("{}" , e.to.0);
+        }).collect();
+        for i in 0..v {
+            if di_res[i] != ans[i] {
+                assert!(false, "not collect");
             }
         }
     }

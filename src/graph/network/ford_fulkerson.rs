8d514a6c605b0736f66c636b7e922a7afc1f1518
Kutimotitokura <kutimoti.tokura@gmail.com>
2018-12-04 19:40:56 +0900

    [add] ford_fulkerson

--- src/graph/network/ford_fulkerson.rs
+++ src/graph/network/ford_fulkerson.rs
@@ -1,2 +1,42 @@
 use graph::*;
 use graph::property::*;
+use graph::network::*;
+use graph::directed_graph::*;
+
+use std::cmp::min;
+
+fn ff_dfs<C: Capacity>(g: &DirectedGraph<usize,Edge>, v: &Vertex, t: &Vertex, zero: C, cap: &mut Vec<C>, used: &mut Vec<bool>, f: C) -> C {
+    if v == t { f }
+    else {
+        used[v.0] = true;
+        for e in g.delta(v) {
+            if !used[e.to.0] && cap[e.index] > zero {
+                let c = min(f, cap[e.index]);
+                let d = ff_dfs(g, &e.to, t, zero, cap, used, c);
+                if d > zero {
+                    cap[e.index] = cap[e.index] -  d;
+                    cap[g.eprop(&e).index] = cap[g.eprop(&e).index] + d;
+                    return d;
+                }
+            }
+        }
+        zero
+    }
+}
+
+pub fn ford_fulkerson<C: Capacity>(net: &mut Network<C>) -> C {
+    let ref mut cap = &mut net.cap;
+    let ref g = & net.g;
+    let s = net.source;
+    let t = net.shink;
+
+    let mut ff = net.zero;
+
+    loop {
+        let mut used = vec![false;g.vertices_cnt()];
+        let f = ff_dfs(&g, &s, &t, net.zero,cap,&mut used, net.inf);
+        if f == net.zero { break; }
+        ff = ff + f;
+    }
+    ff
+}
--- src/graph/network/mod.rs
+++ src/graph/network/mod.rs
@@ -6,30 +6,38 @@ use graph::property::*;
 
 pub struct Network<C: Capacity> {
     pub cap: Vec<C>,
-    pub flow: Vec<C>,
-    pub g: DirectedGraph<usize,Edge>
+    pub g: DirectedGraph<usize,Edge>,
+    pub source: Vertex,
+    pub shink: Vertex,
+    pub zero: C,
+    pub inf: C,
+    pub max_flow: C
 }
 
 impl<C: Capacity> Network<C> {
-    pub fn build<VP: Property, EP: Property, F>(gg: &DirectedGraph<VP,EP>,zero: C,cap_sel: F) -> Network<C>
+    pub fn build<VP: Property, EP: Property, F>(gg: &DirectedGraph<VP,EP>,s: Vertex, t: Vertex ,zero: C, inf: C,cap_sel: F) -> Network<C>
     where F: Fn(&EP) -> C {
         let mut g = DirectedGraph::<usize,Edge>::new(gg.vertices_cnt(),0);
         let mut cap = Vec::<C>::new();
-        let flow = vec![zero;gg.edges_cnt()];
 
         for i in 0..gg.vertices_cnt() {
             for e in gg.delta(&Vertex(i)) {
                 let m = g.edges_cnt();
                 g.add_edge(&e.from, &e.to, Edge {from: e.to, to: e.from, index: m + 1});
-                g.add_edge(&e.from, &e.to, Edge {from: e.from, to: e.to, index: m});
                 cap.push(cap_sel(gg.eprop(e)));
+                g.add_edge(&e.to, &e.from, Edge {from: e.from, to: e.to, index: m});
+                cap.push(zero);
             }
         }
 
         Network {
             cap: cap,
-            flow: flow,
-            g: g
+            g: g,
+            source: s,
+            shink: t,
+            zero: zero,
+            inf,
+            max_flow: zero
         }
     }
 }

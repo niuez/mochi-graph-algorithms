6d8446dbebaa96966bbb18804f62a11151e185cc
Kutimotitokura <kutimoti.tokura@gmail.com>
2018-12-18 23:31:31 +0900

    [update] test

--- src/tests/dijkstra_test.rs
+++ src/tests/dijkstra_test.rs
@@ -4,32 +4,33 @@ use graph::directed_graph::*;
 use graph::shortest_path::bellman_ford::*;
 use graph::shortest_path::dijkstra::*;
 
-#[cfg(test)]
 use graph::*;
 use self::rand::Rng;
 
 #[test]
 fn dijkstra_test() {
-    let v = 100;
-    let e = 300;
-    let mut g = DirectedGraph::<usize, usize>::new(v, 0);
-    for _ in 0..e {
-        let a = rand::thread_rng().gen_range(0, v);
-        let b = rand::thread_rng().gen_range(0, v);
-        let w = rand::thread_rng().gen_range(1, 1001);
-        g.add_edge(&Vertex(a), &Vertex(b), w);
-    }
-    let bf_res = bellman_ford(&g, Vertex(0), |w| w);
-    let di_res = dijkstra(&g, Vertex(0), |w| w);
-    let ans: Vec<Option<usize>> = bf_res
-        .iter()
-        .map(|r| match r {
-            BFResult::Some(w) => Some(*w),
-            _ => None,
-        }).collect();
-    for i in 0..v {
-        if di_res[i] != ans[i] {
-            assert!(false, "not collect");
+    for _ in 0..100 {
+        let v =200;
+        let e = 400;
+        let mut g = DirectedGraph::<usize, usize>::new(v, 0);
+        for _ in 0..e {
+            let a = rand::thread_rng().gen_range(0, v);
+            let b = rand::thread_rng().gen_range(0, v);
+            let w = rand::thread_rng().gen_range(1, 1001);
+            g.add_edge(&Vertex(a), &Vertex(b), w);
+        }
+        let bf_res = bellman_ford(&g, Vertex(0), |w| w);
+        let di_res = dijkstra(&g, Vertex(0), |w| w);
+        let ans: Vec<Option<usize>> = bf_res
+            .iter()
+            .map(|r| match r {
+                BFResult::Some(w) => Some(*w),
+                _ => None,
+            }).collect();
+        for i in 0..v {
+            if di_res[i] != ans[i] {
+                assert!(false, "not collect");
+            }
         }
     }
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,2 +1,4 @@
 #[cfg(test)]
 pub mod dijkstra_test;
+#[cfg(test)]
+pub mod scaling_dijkstra_test;

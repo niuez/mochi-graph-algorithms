9703d532e10fb6328b558587ce6a7623990591f9
Kutimotitokura <kutimoti.tokura@gmail.com>
2019-04-09 18:03:55 +0900

    [third] fix bellman ford

--- src/third/property.rs
+++ src/third/property.rs
@@ -8,9 +8,7 @@ pub trait Zero {
 impl Zero for usize { fn zero() -> Self { 0 } }
 impl Zero for u8 { fn zero() -> Self { 0 } }
 impl Zero for u16 { fn zero() -> Self { 0 } }
-impl Zero for u32 { fn zero() -> Self { 0 } }
-impl Zero for u64 { fn zero() -> Self { 0 } }
-impl Zero for isize { fn zero() -> Self { 0 } }
+impl Zero for u32 { fn zero() -> Self { 0 } } impl Zero for u64 { fn zero() -> Self { 0 } } impl Zero for isize { fn zero() -> Self { 0 } }
 impl Zero for i8 { fn zero() -> Self { 0 } }
 impl Zero for i16 { fn zero() -> Self { 0 } }
 impl Zero for i32 { fn zero() -> Self { 0 } }
--- src/third/single_source_shortest_path/bellman_ford.rs
+++ src/third/single_source_shortest_path/bellman_ford.rs
@@ -18,9 +18,10 @@ where V: Vertex, E: Edge<VType=V> + 'a, AE: AdjEdge<V, E>, G: Directed<'a, V, E,
 
     for _ in 0..n {
         for e in g.edges() {
-            if dist[e.from()] + cost(e.edge()) < dist[e.to()] {
-                dist[e.to()] = W::neg_inf();
-            }
+            dist[e.to()] = 
+                if dist[e.from()] == W::neg_inf() { W::neg_inf() }
+                else if dist[e.from()] + cost(e.edge()) < dist[e.to()] { W::neg_inf() }
+                else { dist[e.to()] }
         }
     }
     
@@ -48,6 +49,22 @@ mod bellman_ford_test {
             assert!(dist[&1] == ArbW::Some(2));
             assert!(dist[&2] == ArbW::Some(-3));
             assert!(dist[&3] == ArbW::Some(-1));
+        }
+        {
+            let mut g = DirectedGraph::new(4);
+            g.add_edge((0, 1, 2));
+            g.add_edge((0, 2, 3));
+            g.add_edge((1, 2, -5));
+            g.add_edge((1, 3, 1));
+            g.add_edge((2, 3, 2));
+            g.add_edge((3, 1, 0));
+
+            let dist = bellman_ford(&g, &0, |e| ArbW::Some(e.2));
+
+            assert!(dist[&0] == ArbW::Some(0));
+            assert!(dist[&1] == ArbW::neg_inf());
+            assert!(dist[&2] == ArbW::neg_inf());
+            assert!(dist[&3] == ArbW::neg_inf());
         }
     }
 }

e402cfc8a105699111f34b22720807506fb66305
Kutimotitokura <kutimoti.tokura@gmail.com>
2019-04-20 18:01:48 +0900

    [comment] graph and property

--- src/graph/kernel/graph.rs
+++ src/graph/kernel/graph.rs
@@ -1,63 +1,128 @@
 use graph::kernel::property::*;
 
+/// Trait for elements of graph (Vertex, Edge, ...) that have ID (usize).
+/// the elements implementing ID are able to use [`graph::kernel::Properties`].
 pub trait ID {
+
+    /// return id of the element.
     fn id(&self) -> usize;
 }
 
+/// Implementing ID for usize.
 impl ID for usize {
+
+    /// return the own value
     fn id(&self) -> usize { *self }
 }
 
+/// Trait for vertices of graphs.
 pub trait Vertex: ID + Eq + Clone { }
 
 impl<V: ID + Eq + Clone> Vertex for V { }
 
+/// Trait for edges of graphs. 
 pub trait Edge {
+
+    /// Vertex type at both ends of edge
     type VType: Vertex;
+
+    /// Start point of edge
     fn from(&self) -> &Self::VType;
+
+    /// End point of edge
     fn to(&self) -> &Self::VType;
 }
 
+/// Implementing Edge for the simple tuple. 
 impl<V> Edge for (V, V) where V: Vertex { 
     type VType = V;
     fn from(&self) -> &Self::VType { &self.0 }
     fn to(&self) -> &Self::VType { &self.1 }
 }
 
+/// Implementing Edge for the simple tuple. 
 impl<V, P> Edge for (V, V, P) where V: Vertex, P: Property { 
     type VType = V;
     fn from(&self) -> &Self::VType { &self.0 }
     fn to(&self) -> &Self::VType { &self.1 }
 }
 
+/// Trait for adjacency edges of graph.
+/// Why do we use [`Edge`] as is? There are 2 reasons.
+/// - To give values to the edges to use Properties (AdjEdge has ID).
+/// - When using a undirected graph as a directed graph, must swap two ends of edge.
 pub trait AdjEdge: ID + Edge {
+
+    /// Edge type of raw edge.
     type EType: Edge<VType=Self::VType>;
+
+    /// return raw edge.
     fn edge(&self) -> &Self::EType;
 }
 
+/// Trait for adjcency edges on ResidualNetwork.
+/// It has reverse edge.
 pub trait ResidualEdge: AdjEdge {
     fn rev(&self) -> Self;
 }
 
+/// Trait of graph.
 pub trait Graph<'a> {
+    /// Type of vertices.
     type VType: Vertex + 'a;
+
+    /// Type of edges.
     type EType: Edge<VType=Self::VType>;
+
+    /// Type of adjacency edges.
     type AEType: AdjEdge<VType=Self::VType, EType=Self::EType>;
+
+    /// Type of iterator for adjacency list.
     type AdjIter: std::iter::Iterator<Item=Self::AEType>;
+
+    /// Type of iterator for edges list.
     type EIter: std::iter::Iterator<Item=Self::AEType>;
+
+    /// Type of iterator for vertices list.
     type VIter: std::iter::Iterator<Item=&'a Self::VType>;
+
+    /// return adjacency list from the vertex v.
     fn delta(&'a self, v: &Self::VType) -> Self::AdjIter;
+
+    /// return edges list.
     fn edges(&'a self) -> Self::EIter;
+
+    /// return vertices list.
     fn vertices(&'a self) -> Self::VIter;
+
+    /// return the number of vertices.
     fn v_size(&self) -> usize;
+
+    /// return the number of edges.
     fn e_size(&self) -> usize;
 }
 
+/// Trait of directed graph.
 pub trait Directed<'a>: Graph<'a> {}
+
+/// Trait of undirected graph.
+/// graphs implementing this hold that the edge `(v, u)` exists for the edge `(u, v)` when the graph
+/// use as directed graph
 pub trait Undirected<'a>: Graph<'a> {}
+
+/// Trait of bipartite graph.
 pub trait Bipartite<'a>: Undirected<'a> {
+
+    /// Type of iterator for vertices in one side.
     type BVIter: std::iter::Iterator<Item=&'a Self::VType>;
+
+    /// return vertices list in left side.
     fn left_vertices(&'a self) -> Self::BVIter;
+
+    /// return vertices list in right side.
     fn right_vertices(&'a self) -> Self::BVIter;
 }
+
+/// Trait of residual network
+/// `AEType` must be `ResidualEdge`.
 pub trait Residual<'a>: Directed<'a> where <Self as Graph<'a>>::AEType: ResidualEdge {}
--- src/graph/kernel/property/mod.rs
+++ src/graph/kernel/property/mod.rs
@@ -1,29 +1,48 @@
 pub mod literal;
 
+/// Trait for properties.
 pub trait Property: Copy {}
 
 impl<P> Property for P where P: Copy {}
 
+/// Types implementing `ToNNegWeight` are able to convert to non-negative weights.
+/// This trait use the algorithms with potentials (`dijkstra_with_potential`, etc...).
 pub trait ToNNegWeight {
+
+    /// converting type.
     type Output: NNegWeight;
+
+    /// convert to non-negative weights.
     fn to_nnegw(&self) -> Self::Output;
 }
 
+/// Types implementing `ToARbWeight` are able to convert to arbitrary weights.
+/// This trait use to reverse from non-negative weight after converting weight.
 pub trait ToArbWeight {
+
+    /// converting type.
     type Output: ArbWeight;
+
+    /// convert to non-negative weights.
     fn to_arbw(&self) -> Self::Output;
 }
 
+/// Trait of arbitrary weights.
+/// the arbirary weight has infinity, zero and negative infinity.
 pub trait ArbWeight where Self: ToNNegWeight + ToArbWeight + Property + std::ops::Add<Output=Self> + std::ops::Sub<Output=Self> + std::cmp::Ord {
     fn inf() -> Self;
     fn zero() -> Self;
     fn neg_inf() -> Self { unreachable!() }
 }
 
+/// Trait of non-negative weights.
 pub trait NNegWeight where Self: ArbWeight {}
 
+/// Trait of weights of integer. 
+/// types implementing this use the scaling algorithms.
 pub trait IntegerWeight: ArbWeight + std::ops::Shl<usize, Output=Self> + std::ops::Shr<usize, Output=Self> {}
 
+/// Trait of capacity for maxflow, mcf, and so on.
 pub trait Capacity: NNegWeight + IntegerWeight {}
 
 impl<W> Capacity for W where W: NNegWeight + IntegerWeight {}

885e3e9e4b80cecf8a9c3ccbe2adfed46275b987
Kutimotitokura <kutimoti.tokura@gmail.com>
2019-04-21 12:57:21 +0900

    [delete] adjedge_graph an subedge_graph

--- src/graph/graph/mod.rs
+++ src/graph/graph/mod.rs
@@ -2,12 +2,8 @@ pub mod directed_graph;
 pub mod undirected_graph;
 pub mod bipartite_graph;
 pub mod residual_network;
-pub mod subedge_graph;
-pub mod adjedge_graph;
 
 pub use self::directed_graph::DirectedGraph;
 pub use self::undirected_graph::UndirectedGraph;
 pub use self::bipartite_graph::BipartiteGraph;
 pub use self::residual_network::ResidualNetwork;
-pub use self::subedge_graph::SubEdgeGraph;
-pub use self::adjedge_graph::AdjEdgeGraph;

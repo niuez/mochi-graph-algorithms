edc6fd230e6db4f0f3d513bdf96942e770b52992
kutimoti <kutimoti.tokura@gmail.com>
2019-06-08 00:44:11 +0900

    [fix] dinic use peekable

--- src/graph/algorithm/maxflow/dinic.rs
+++ src/graph/algorithm/maxflow/dinic.rs
@@ -7,19 +7,21 @@ use graph::algorithm::single_source_shortest_path::bfs;
 
 use std::cmp::min;
 
-fn dinic_dfs<'a, N, C>(g: &'a N, v: &N::VType, t: &N::VType, cap: &mut Properties<C>, level: &Properties<NNegW<usize>>, f: C) -> C 
+fn dinic_dfs<'a, N, C>(g: &mut Vec<std::iter::Peekable<N::AdjIter>>, v: &N::VType, t: &N::VType, cap: &mut Properties<C>, level: &Properties<NNegW<usize>>, f: C) -> C 
 where N: Residual<'a>, N::AEType: ResidualEdge, C: Capacity {
     if v == t { f }
     else {
         let mut flow = C::zero();
-        for ref e in g.delta(v) {
-            if cap[e] > C::zero() && level[e.from()] < level[e.to()] {
-                let c = min(f - flow, cap[e]);
-                let d = dinic_dfs(g, e.to(), t, cap, level, c);
+        while let Some(ref e) = g[v.id()].peek().cloned() {
+            let c = min(f - flow, cap[e]);
+            if c > C::zero() && level[e.from()] < level[e.to()] {
+                let d = dinic_dfs::<N, C>(g, e.to(), t, cap, level, c);
                 cap[e] = cap[e] - d;
                 cap[&e.rev()] = cap[&e.rev()] + d;
                 flow = flow + d;
+                if f == flow { break; }
             }
+            g[v.id()].next();
         }
 
         flow
@@ -39,7 +41,8 @@ where N: Residual<'a>, N::AEType: ResidualEdge, C: Capacity, F: Fn(&N::AEType) -
         level = bfs(&sg, s);
         match level[t] { NNegW::Inf => false, _ => true }
     } {
-        ff = ff + dinic_dfs(g, s, t, &mut rcap, &level, C::inf());
+        let mut g_peek = g.vertices().map(|v| g.delta(v).peekable()).collect();
+        ff = ff + dinic_dfs::<N, C>(&mut g_peek, s, t, &mut rcap, &level, C::inf());
     }
 
     ff
